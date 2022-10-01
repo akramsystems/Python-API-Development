a81d418a87092cfcbd57a5fa5f20102899eb29f1
Ali Akram <akramsystems@gmail.com>
2022-10-01 00:01:49 -0400

    push to ubuntu

--- app/main.py
+++ app/main.py
@@ -37,4 +37,4 @@ app.include_router(vote.router)
 
 @ app.get("/")
 async def root():
-    return {"message": "This is the main page, by Ali Akram,\n\n deploy from CI/CD pipeline"}
+    return {"message": "This is the main page, by Ali Akram,\n\n deploy from CI/CD pipeline to Ubuntu"}
